f8c3b810811fc897b45401d29a33765785f6acc2
Eryk <erykurbas2@gmail.com>
2017-03-24 13:57:03 +0100

    Basic program

---
+++ WiecznyKalendarz.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void)
+{
+	char znak; 
+	int rok, mies, dz, spr;
+
+	do
+    {
+		
+        printf("\n Wieczny kalendarz - podaje dzien tygodnia dla danej daty\n");
+        do
+        {
+            fflush(stdin);
+            printf("\n Podaj rok: ");
+            spr = scanf("%d", &rok);
+            printf(" wartosc zwrocona przez scanf(): %d\n", spr);
+        }while(!spr);
+        do
+        {
+            printf(" Podaj miesiac: ");
+            spr = scanf("%d", &mies);
+            fflush(stdin);
+            printf(" wartosc zwrocona przez scanf(): %d\n", spr);
+        }while(!((mies > 0) && (mies <=12)));
+        printf(" Podaj dzien: ");
+        scanf("%d", &dz);
+
+        printf("\n Wybrana data: %d-%02d-%02d", rok, mies, dz);
+
+        if(mies<3)
+        {
+            rok--;
+            mies += 12;
+        }
+
+        dz=rok+rok/4-rok/100+rok/400+3*mies-((mies*2+1)/5)+dz+1;
+        spr = (dz/7)*7;
+        dz=dz-spr;
+
+
+        switch (dz)
+        {
+            case 0:
+                printf(" Niedziela");
+                break;
+            case 6:
+                printf(" Sobota");
+                break;
+            case 5:
+                printf(" Piatek");
+                break;
+            case 4:
+                printf(" Czwartek");
+                break;
+            case 3:
+                printf(" Sroda");
+                break;
+            case 2:
+                printf(" Wtorek");
+                break;
+            case 1:
+                printf(" Poniedzialek");
+                break;
+        }
+        printf(" zmienna spr=%d\n",spr);
+        printf("\n\n");
+		printf("Powtorzyc t/n? : ");
+		//fflush(stdin);
+	    scanf("%s", &znak);
+		
+	}while(znak == 't');
+
+
+	return 0;
+}
+
